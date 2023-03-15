d1a887c9ef828d3ae4a72062d54bd0bd79ca6042
FionaN <fiona1941nicdao@gmail.com>
2023-03-15 17:38:03 -0500

    add content for homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,14 @@
-
+import earth from "../../assets/images/nasa-earth-nobackground.png"
 export default function Home(){
     return(
-        <section>
-            <h1>Home page</h1>
-            <h1>My Journey Around the World</h1>
+        <section className="home-page">
+            <h1 className="home-page-title">My Journey Around the World</h1>
+            <div className="image-container"> 
+                <img className="earth" alt="earth" src={earth}/>
+            </div>
+            <p className="home-page-content">
+                Hi! This is my personal website. Here I test my new progamming skills, blog about my life, talk about new things I learn, and a little bit of every. So join me in my journy around the world and living and hopefully thriving.  
+            </p>
         </section>
     )
 }
