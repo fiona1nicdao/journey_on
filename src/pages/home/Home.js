eacd257f07b21d252b6f3e686603e823a9157b9a
FionaN <fiona1941nicdao@gmail.com>
2023-03-16 10:13:27 -0500

    fixed breakpoints for homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,12 +3,15 @@ export default function Home(){
     return(
         <section className="home-page">
             <h1 className="home-page-title">My Journey Around the World</h1>
-            <div className="image-container"> 
-                <img className="earth" alt="earth" src={earth}/>
+            <div className="container">
+                <div className="image-container"> 
+                    <img className="earth" alt="earth" src={earth}/>
+                </div>
+                <p className="home-page-content">
+                    Hi! This is my personal website. Here I test my new progamming skills, blog about my life, talk about new things I learn, and a little bit of every. So join me in my journy around the world and living and hopefully thriving.  
+                </p>
             </div>
-            <p className="home-page-content">
-                Hi! This is my personal website. Here I test my new progamming skills, blog about my life, talk about new things I learn, and a little bit of every. So join me in my journy around the world and living and hopefully thriving.  
-            </p>
+
         </section>
     )
 }
