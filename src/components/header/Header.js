5ce44eab1bff2cbdcccecfe86e7e110911f8103e
FionaN <fiona1941nicdao@gmail.com>
2023-04-28 12:18:33 -0500

    not finished fixing the nav bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,21 @@
 import { NavLink } from 'react-router-dom';
+import NavBar from '../NavBar';
 
 export default function Header(){
     return(
         <header className='header'>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/travel'>Travel</NavLink>
-            <NavLink to='/code'>Code</NavLink>
-            <NavLink to='/test'>Test</NavLink>
-
+            <div className="dropdown">
+                <button className='dropdown-menu-button'>
+                    <i className="menu-icon fa-solid fa-bars" ></i>
+                </button>
+                <div className='menu'>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/travel'>Travel</NavLink>
+                    <NavLink to='/code'>Code</NavLink>
+                    <NavLink to='/test'>Test</NavLink>
+                </div>
+            </div>
+            <NavBar/>
         </header>
     )
 }
